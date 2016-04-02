8a0ddd06b52e757508b71182cdc02aeacaeed069
Sam Collard <sam.collard@softwire.com>
2016-04-02 18:16:34 +0100

    Finish chat room functionality
    Also added cookies lib while there.

--- app.js
+++ app.js
@@ -4,24 +4,44 @@ require('dotenv').config()
 
 GLOBAL._ = require('lodash')
 
+var path = require('path')
+
 var express = require('express')
 var exphbs = require('express-handlebars')
-
 var app = express()
+
 var http = require('http').Server(app)
+
 var io = require('socket.io')(http)
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
 
 app.use('/', require('./routes/home'))
 app.use('/room', require('./routes/room'))
 
+var users = room => _(io.sockets.connected)
+  .filter(['room', room])
+  .map('user')
+  .value()
+
 io.on('connection', function (socket) {
-  console.log('connected')
-  socket.on('chat message', function (chatMessage) { console.log(chatMessage.room + ': ' + chatMessage.message) })
+  socket.on('joined room', function(details) {
+    _.assign(socket, _.pick(details, 'room', 'user'))
+    socket.emit('user list', users(details.room))
+    io.emit(socket.room + ' user list', { users: users(details.room), owner: '' })
+  })
+
+  socket.on('chat message', function (chatMessage) {
+    console.log(socket.room + ': ' + chatMessage.message)
+    socket.broadcast.emit(socket.room + ' new message', {
+      user: socket.user,
+      message: chatMessage.message
+    })
+  })
 })
 
 http.listen(3000, () => { console.log('Listening on *:3000') })
--- public/script/room.js
+++ public/script/room.js
@@ -4,16 +4,42 @@ $(document).ready(function () {
   var socket = io()
 
   var room = window.location.pathname.match(/\/room\/([a-z]+-[a-z]+-[a-z]+)/)[1]
+  var user = prompt('Please enter a name')
+
+  socket.on(room + ' user list', updateUsers)
+  socket.on(room + ' new message', showMessage)
+
+  socket.on('test', x => console.log(x))
+
+  function updateUsers(roomInfo) {
+    var userDivs = _(roomInfo.users)
+      .sort()
+      .map(user => `<div class="player ${user === roomInfo.owner ? 'owner' : ''}">${user}</div>`)
+      .value()
+    $('#playerList').html(userDivs)
+  }
+
+  function showMessage(chatMessage) {
+    var chatContent = $('#chatContent')
+    chatContent.append(`
+      <div class="message">
+        ${chatMessage.user}: ${chatMessage.message}
+      </div>
+    `)
+    chatContent[0].scrollTop = chatContent[0].scrollHeight
+  }
 
   $('form.chat-entry').submit(function () {
     var chatInput = $('#chatInput')
     var message = chatInput.val()
     if (message) {
-      socket.emit('chat message', { room, message })
+      socket.emit('chat message', { message })
       chatInput.val('')
+      showMessage({message, user})
     }
 
     return false
   })
-})
 
+  socket.emit('joined room', { room, user })
+})

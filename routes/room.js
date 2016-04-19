9e5c00e912eb958b95896765adf1ec75679d2b35
CheshireSwift <swift@swifteagle.co.uk>
2016-04-19 21:46:42 +0100

    Display draft room when started

--- routes/room.js
+++ routes/room.js
@@ -3,21 +3,26 @@
 var router = require('express').Router()
 var rooms = require('../roomManager')
 
-router.get('/', (req, res) => {
-  var keyphrase = req.query.keyphrase
-  if (!keyphrase) {
+function validateKeyphrase(req, res) {
+  var keyphrase = req.params.keyphrase || req.query.keyphrase
+  if (!keyphrase || !/^[a-z]+-[a-z]+-[a-z]+$/.test(keyphrase)) {
     res.status(400).render('bad_key')
+    return false
   }
+
+  return keyphrase
+}
+
+router.get('/', (req, res) => {
+  var keyphrase = validateKeyphrase(req, res)
+  if (!keyphrase) { return }
 
   res.redirect('/room/' + keyphrase.replace(/ /g, '-'))
 })
 
 router.get('/:keyphrase', (req, res) => {
-  var keyphrase = req.params.keyphrase
-  if (!/^[a-z]+-[a-z]+-[a-z]+$/.test(keyphrase)) {
-    res.status(400).render('bad_key')
-    return
-  }
+  var keyphrase = validateKeyphrase(req, res)
+  if (!keyphrase) { return }
 
   var room = rooms.get(keyphrase)
   if (!room) {
@@ -25,7 +30,21 @@ router.get('/:keyphrase', (req, res) => {
     return
   }
 
-  res.render('room', _.merge({}, room, {stylesheet: 'room', script: 'room'}))
+  var template = room.started ? 'draft' : 'room'
+  res.render(template, _.merge({},
+    room,
+    {stylesheet: template, script: template},
+    {isOwner: room.isOwner(req.cookies['drafting-swiftly_username'])}
+  ))
+})
+
+router.get('/:keyphrase/start', (req, res) => {
+  var keyphrase = validateKeyphrase(req, res)
+  if (!keyphrase) { return }
+
+  var room = rooms.get(keyphrase)
+  room.started = true
+  res.redirect('/room/' + keyphrase)
 })
 
 module.exports = router
